625d79e95e7c89b146df15f7ca9c2175bdb9e324
marvelcollin <marvelcollin7@gmail.com>
2026-03-05 14:34:22 +0700

    fix: authentication stuck flick

--- app/components/layouts/navbar-layout.tsx
+++ app/components/layouts/navbar-layout.tsx
@@ -1,10 +1,25 @@
 import Navbar from "../navbar";
 import { Sidebar } from "~/components/ui/sidebar";
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import SidebarContent from "~/components/sidebar/sidebar-content";
 import CenterLayout from "~/components/layouts/center-layout";
+import { useAuth } from "~/lib/auth";
+import { useEffect } from "react";
 
 const NavbarLayout = () => {
+  const { user, loading } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      navigate("/", { replace: true });
+    }
+  }, [loading, user]);
+
+  if (loading) return null;
+
+  if (!user) return null;
+
   return (
     <div className="flex flex-col min-h-screen w-full box-border bg-gray-50">
       <Navbar />
